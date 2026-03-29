32592cd3d0be7b764a6c8887250da72b31e69a63
muhammedhanan9000 <mhdhananiqbal@gmail.com>
2026-03-29 19:04:26 +0530

    aa

--- service/work.js
+++ service/work.js
@@ -534,7 +534,270 @@ const printer = new PdfPrinter(fonts);
       });
   });
 
-  
+     app.post("/ad/excel", upload.none(), function (req, res) {
+       var LogId = req.LogID;
+   
+    var Client = JSON.parse(req.body?.Client);
+    var date = req.body.date;
+ 
+    var tvpClient = new sql.Table();
+        tvpClient.columns.add("ID", sql.Int);
+ 
+    if (Array.isArray(Client)) {
+        Client.forEach(client => tvpClient.rows.add(client));
+      }
+ 
+ 
+    connection
+      .then((pool) => {
+        return pool
+          .request()
+          .input("LogID", sql.Int, LogId)
+          .input("Client", tvpClient)
+          .input("date", sql.NVarChar(10), date)
+          .execute("PROCAdList");
+      })
+      .then((result) => {
+ 
+ 
+      console.log(result.recordsets[0]);
+      var works = result.recordsets[0]
+ 
+      var workbook = excelbuilder.createWorkbook('./uploads/Excel/', 'Ads'  + '.xlsx')
+      var sheet1 = workbook.createSheet('Sheet', 6, works.length + 1);
+        // Fill some data
+        // sheet1.set('Code','Name','UOM','SystemStock','PhysicalStock','Variation');
+        sheet1.set(1, 1, 'Ad');
+        sheet1.set(2, 1, 'Client');
+        sheet1.set(3, 1, 'Date period');
+        sheet1.set(4, 1, 'Spend');
+        sheet1.set(5, 1, 'No.of conversions');
+        sheet1.set(6, 1, 'Revenue');
+ 
+ 
+        sheet1.font(1, 1, { sz: '12', family: '3', bold: 'true' })
+        sheet1.font(2, 1, { sz: '12', family: '3', bold: 'true' })
+        sheet1.font(3, 1, { sz: '12', family: '3', bold: 'true' })
+        sheet1.font(4, 1, { sz: '12', family: '3', bold: 'true' })
+        sheet1.font(5, 1, { sz: '12', family: '3', bold: 'true' })
+        sheet1.font(6, 1, { sz: '12', family: '3', bold: 'true' })
+       
+        sheet1.border(1, 1, { left: 'thin', top: 'thin', right: 'thin', bottom: 'thin' });
+        sheet1.border(2, 1, { left: 'thin', top: 'thin', right: 'thin', bottom: 'thin' });
+        sheet1.border(3, 1, { left: 'thin', top: 'thin', right: 'thin', bottom: 'thin' });
+        sheet1.border(4, 1, { left: 'thin', top: 'thin', right: 'thin', bottom: 'thin' });
+        sheet1.border(5, 1, { left: 'thin', top: 'thin', right: 'thin', bottom: 'thin' });
+        sheet1.border(6, 1, { left: 'thin', top: 'thin', right: 'thin', bottom: 'thin' });
+   
+ 
+        for (var j = 0; j < works.length; j++) {
+ 
+ 
+            sheet1.set(1, j + 2, works[j].title);
+            sheet1.set(2, j + 2, works[j].client);
+            sheet1.set(3, j + 2, works[j].dateperiod);
+            sheet1.set(4, j + 2, works[j].spend);
+            sheet1.set(5, j + 2, works[j].count);
+            sheet1.set(6, j + 2, works[j].Revenue);
+           
+ 
+            sheet1.border(1, j + 2, { left: 'thin', top: 'thin', right: 'thin', bottom: 'thin' });
+            sheet1.border(2, j + 2, { left: 'thin', top: 'thin', right: 'thin', bottom: 'thin' });
+            sheet1.border(3, j + 2, { left: 'thin', top: 'thin', right: 'thin', bottom: 'thin' });
+            sheet1.border(4, j + 2, { left: 'thin', top: 'thin', right: 'thin', bottom: 'thin' });
+            sheet1.border(5, j + 2, { left: 'thin', top: 'thin', right: 'thin', bottom: 'thin' });
+            sheet1.border(6, j + 2, { left: 'thin', top: 'thin', right: 'thin', bottom: 'thin' });
+        }
+ 
+        workbook.save(function (ok) {
+ 
+            if (ok == false)
+                workbook.cancel();
+            else
+                //
+                res.json({ "result": 'success',filename:'Ads.xlsx' });
+        });
+ 
+ 
+ 
+        // res.json({
+        //   result: "success",
+        //   data: result.recordsets,
+        // });
+      })
+      .catch((err) => {
+        console.log("SQL Error:", err);
+ 
+        res.status(500).json({
+          result: "failed",
+          error: err.message,
+        });
+      });
+  });
+ 
+  app.post("/ad/pdf", upload.none(), function (req, res) {
+    var LogId = req.LogID;
+   console.log(req.body);
+   
+    var Client = JSON.parse(req.body?.Client);
+    var date = req.body.date;
+ 
+    var tvpClient = new sql.Table();
+        tvpClient.columns.add("ID", sql.Int);
+ 
+    if (Array.isArray(Client)) {
+        Client.forEach(client => tvpClient.rows.add(client));
+      }
+ 
+ 
+    connection
+      .then((pool) => {
+        return pool
+          .request()
+          .input("LogID", sql.Int, LogId)
+          .input("Client", tvpClient)
+          .input("date", sql.NVarChar(10), date)
+          .execute("PROCAdList");
+      })
+      .then((result) => {
+ var data = result.recordsets[0]
+                        var Header1 = [];
+                       
+                        Header1.push({ text: 'Title',   style: 'tableHeader', alignment: 'center' } )
+                        Header1.push({ text: 'Client',   style: 'tableHeader', alignment: 'center' })
+                        Header1.push({ text: 'Date Period',   style: 'tableHeader', alignment: 'center' })
+                        Header1.push({ text: 'Spend',   style: 'tableHeader', alignment: 'center' })
+                        Header1.push({ text: 'No.of conversions',   style: 'tableHeader', alignment: 'center' })
+                        Header1.push({ text: 'Revenue',   style: 'tableHeader', alignment: 'center' })
+ 
+ 
+                        var head=['title','client','dateperiod','spend','count','Revenue']
+                        var bodyData = [];
+                        bodyData.push(Header1)
+                        data.forEach(function (sourceRow) {
+                            var dataRow = []
+                            head.forEach(function (key, index) {
+                                var align = 'center'
+                                if (typeof sourceRow[key] == 'number') {
+                                    sourceRow[key] = sourceRow[key].toFixed(2)
+                                    align = 'right'
+                                }
+                                if (key != '$$hashKey') {
+                                    dataRow.push({ text: sourceRow[key].toString(), style: 'tableData', alignment: align, colSpan: 1 })
+ 
+                                }
+                            })
+                            bodyData.push(dataRow)
+                        });
+ 
+ 
+ 
+                        var dd = {
+                            pageSize: 'A4',
+                            pageOrientation: 'landscape',
+                            pageMargins: [10, 40, 40, 60],
+                            footer: {
+                                columns: [
+                                    { text: 'Powered By ', style: 'reportFooter', alignment: 'right' }
+                                ]
+                            },
+                            content: [
+                           
+                            { text: 'Ads and Conversions', style: 'header', alignment: 'center' },
+                     
+                            {
+                                style: 'tableExample',
+                                table: {
+                                    headerRows: 1,
+                                    widths: ['*', '*', '*', '*', '*', '*'],
+ 
+                                    body: bodyData
+                                }
+                            }
+ 
+                            ],
+                            styles: {
+                                header: {
+                                    fontSize: 25,
+                                    bold: true,
+                                    margin: [0, 0, 0, 0]
+                                },
+                                subheader: {
+                                    fontSize: 16,
+                                    bold: true,
+                                    margin: [0, 0, 0, 0]
+                                },
+                                tableExample: {
+                                    margin: [0, 0, 0, 0]
+                                },
+                                tableExample1: {
+                                    fontSize: 20,
+                                    margin: [240, 5, 0, 15]
+                                },
+                                tableHeader: {
+                                    bold: true,
+                                    fontSize: 10,
+                                    color: 'black',
+                                    margin: [0, 0, 0, 0]
+                                },
+                                tablesubHeader: {
+                                    bold: true,
+                                    fontSize: 8,
+                                    color: 'black',
+ 
+                                },
+                                tableData: {
+                                    fontSize: 6,
+                                    color: 'black'
+                                },
+                                tableData1: {
+                                    fontSize: 20,
+                                    color: 'black'
+                                },
+                                reportFooter: {
+                                    bold: true,
+                                    fontSize: 6,
+                                    color: 'grey',
+                                    margin: [0, 25, 15, 0]
+                                },
+ 
+                                searchCrit: {
+                                    fontSize: 10,
+                                    color: 'black'
+                                },
+                            }
+                        }
+                       
+ 
+ 
+                        var pdfDoc = printer.createPdfKitDocument(dd);
+                        pdfDoc.pipe(fs.createWriteStream('uploads/PDF/ads.pdf')).on('finish', function () {
+                            //success
+                            console.log("create the file")
+                            res.json({ "result": "success" ,filename:'ads.pdf'})
+                        });
+                        pdfDoc.end()
+ 
+           
+                       
+ 
+ 
+ 
+        // res.json({
+        //   result: "success",
+        //   data: result.recordsets,
+        // });
+      })
+      .catch((err) => {
+        console.log("SQL Error:", err);
+ 
+        res.status(500).json({
+          result: "failed",
+          error: err.message,
+        });
+      });
+  });
+ 
  
   
 };
